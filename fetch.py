7de15d2fafbd61525dec8b516274e853d3d074b0
Simon Kirkby <obeygiantrobot@gmail.com>
2020-01-31 09:28:38 +0800

    added boneless

--- fetch.py
+++ fetch.py
@@ -1,28 +1,36 @@
-# nmigen fetcher 
+# nmigen fetcher
 from git import Repo
 import os
 
 repos = {
-    'nmigen' :'https://github.com/m-labs/nmigen',
-    'nmigen-soc' :'https://github.com/m-labs/nmigen-soc',
-    'nmigen-boards' :'https://github.com/m-labs/nmigen-boards',
+    "nmigen": "https://github.com/m-labs/nmigen",
+    "nmigen-soc": "https://github.com/m-labs/nmigen-soc",
+    "nmigen-boards": "https://github.com/m-labs/nmigen-boards",
+    "nmigen-stdio": "https://github.com/m-labs/nmigen-stdio",
+    "minerva": "https://github.com/lambdaconcept/minerva.git",
+    "Boneless-CPU": "https://github.com/whitequark/Boneless-CPU",
 }
+
 
 def Fetch(item):
     name = item[0]
     url = item[1]
-    path = 'lib'+os.sep+name
-    print(name,url,path)
+    path = "lib" + os.sep + name
+    print(name, url, path)
     try:
         r = Repo(path)
     except:
         print(name + " does not exist , creating")
         r = Repo.init(path)
-        r.create_remote('origin',url)
-        r.remotes.origin.pull('master')
-
+        r.create_remote("origin", url)
+        r.remotes.origin.pull("master")
     return r
 
-local_repos = []
-for  i in repos.items():
-    local_repos.append(Fetch(i))
+
+def FetchAll():
+    local_repos = []
+    for i in repos.items():
+        local_repos.append(Fetch(i))
+
+
+FetchAll()
