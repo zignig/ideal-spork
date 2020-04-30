c3c2fdbc4905c90570c95e58b18537467ac921c3
Simon Kirkby <obeygiantrobot@gmail.com>
2020-04-30 09:15:46 +0800

    add framework for spork cli command

--- setup.py
+++ setup.py
@@ -6,4 +6,13 @@ setup(
     author="zignig",
     description="Boneless v3 , and peripherals",
     packages=find_packages(),
+    project_urls={
+        "Source Code": "https://github.com/zignig/ideal_spork",
+        "Bug Tracker": "https://github.com/zignig/ideal_spork/issues",
+    },
+    entry_points={
+        "console_scripts": [
+            "spork = ideal_spork.cli:as_main",
+        ]
+    }
 )
